f9762ee9c966dfaa4e1bd6437fac9bd86501e4e9
Karen Nino <karen.n@miro.com>
2023-02-03 11:40:06 -0500

    Refactoring building to support static pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import './custom.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+const Content = () => (
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>
-);
+)
 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Content />, rootElement);
+} else {
+  render(<Content />, rootElement);
+}
