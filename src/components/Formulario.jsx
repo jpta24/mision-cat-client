c04382fa8a5fa96348ff51c65767372bf5c36236
Jpta24 <jeanpierretorres@gmail.com>
2024-01-31 20:36:26 +0100

    Fixed intentions

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,71 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function Formulario() {
-  return (
-    <form className='col-8 mx-auto h5'>
-        <div >
-            <label htmlFor="nombre" className='col-4'>Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required  className='col-8 my-1'/>
-        </div>
-        <div >
-            <label htmlFor="correo" className='col-4'>Correo Electrónico:</label>
-            <input type="email" id="correo" name="correo" required className='col-8 my-1'/>
-        </div>
-        <div >
-        <label htmlFor="nombre" className='col-4 '>Intenciones:</label>
-            <input type="text" id="nombre" name="nombre" required  className='col-8 my-1'/>
-        </div>
-        <div className='d-flex align-items-start'>
-            <label htmlFor="intenciones" className='col-4'>Intenciones Por:</label>
-            <textarea id="intenciones" name="intenciones" placeholder="Intenciones por:" rows="4" required className='col-8 my-1'></textarea>
-        </div>
-        <div>
-        <Button variant='primary' className='mx-2 avisos col-5 mx-auto my-2'>Enviar Intención</Button>
-        </div>
-        </form>
-
-  )
+	return (
+		<form className='col-8 mx-auto h5'>
+			<div>
+				<label htmlFor='nombre' className='col-4'>
+					Nombre:
+				</label>
+				<input
+					type='text'
+					id='nombre'
+					name='nombre'
+					required
+					className='col-8 my-1'
+				/>
+			</div>
+			<div>
+				<label htmlFor='correo' className='col-4'>
+					Correo Electrónico:
+				</label>
+				<input
+					type='email'
+					id='correo'
+					name='correo'
+					required
+					className='col-8 my-1'
+				/>
+			</div>
+			{/* <div>
+				<label htmlFor='nombre' className='col-4 '>
+					Intenciones:
+				</label>
+				<input
+					type='text'
+					id='nombre'
+					name='nombre'
+					required
+					className='col-8 my-1'
+				/>
+			</div> */}
+			<div className='d-flex align-items-start'>
+				<label htmlFor='intenciones' className='col-4'>
+					Intenciones Por:
+				</label>
+				<textarea
+					id='intenciones'
+					name='intenciones'
+					placeholder='Intenciones por:'
+					rows='3'
+					required
+					className='col-8 my-1'
+				></textarea>
+			</div>
+			<div className='d-flex'>
+				<div className='col-4'></div>
+				<div className='col-8'>
+					<Button
+						variant='primary'
+						className='mx-2 avisos col-6 my-2'
+					>
+						Enviar Intención
+					</Button>
+				</div>
+			</div>
+		</form>
+	);
 }
 
-export default Formulario
+export default Formulario;
